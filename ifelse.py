f01f11c6d9953782b49f7c3ab87e65ff19d8cc8c
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-03-09 01:01:28 +0530

    Initial commit

---
+++ ifelse.py
@@ -0,0 +1,7 @@
+# Q1: Take a number from user and print "Even" if the number is even otherwise print "Odd".
+
+number = int(input("Enter a number"))
+if (number%2==0):
+  print(f"The given numbber {number} is even")
+else:
+    print(f"The given numbber {number} is odd")
