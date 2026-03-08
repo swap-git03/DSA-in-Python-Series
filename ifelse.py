c85f687330bbb2254adf7f3e9b71ad08bf0cfadd
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-03-09 01:01:51 +0530

    /

--- ifelse.py
+++ ifelse.py
@@ -5,3 +5,11 @@ if (number%2==0):
   print(f"The given numbber {number} is even")
 else:
     print(f"The given numbber {number} is odd")
+
+# Q2: Take two numbers from the user and print which number is larger.
+num1 = (input("Enter 1st number"))
+num2 = (input("Enter 2nd number"))
+if (num1>num2):
+  print(f"{num1}>{num2}")
+else:
+  print(f"{num2}>{num1}")
