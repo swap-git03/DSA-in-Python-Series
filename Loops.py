34c89c39097caf6b90ab4c73615489396a25a195
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-03-11 17:34:40 +0530

    /

--- Loops.py
+++ Loops.py
@@ -2,3 +2,9 @@
 n = int(input("Enter a number"))
 for i in range(1,n):
   print(i)
+
+
+# Q11: Take a number from the user and print the multiplication table of that number from 1 to 10.
+number = int(input("NEter your nukmber"))
+for i in range(1,11):
+   print(number*i)
