8919221108683c161cc53e6ec8cad870911708c3
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-04-22 10:45:41 +0530

    /

--- Builtin.py
+++ Builtin.py
@@ -39,3 +39,25 @@ print("Lenght of num is",len(nums));
 print("Maximum length of num is", max(nums));
 print("Minimum length of num is", min(nums));
 print("Sum of numbers is", sum(nums));
+
+# 2.Take a list of numbers and print:
+# 1. sorted list
+# 2. original list
+
+nums = list(map(int, input("Enter your num : ").split()))
+print(f"The original numbers are{nums}")
+print(f"The sorted numbers are{sorted(nums)}")
+
+# Q3: Take a list of numbers and print the largest and second largest number. 
+nums = list(map(int, input("Enter a number : ").split())) 
+maximum = max(nums) 
+print(maximum) 
+nums.remove(maximum) 
+second_maximum = max(nums) 
+print(second_maximum)
+
+# Q4: Take a list of numbers and print only the even numbers from the list.
+nums = list(map(int, input("Enter your number : ").split()))
+for i in nums:
+    if i % 2 == 0:
+        print(i);
