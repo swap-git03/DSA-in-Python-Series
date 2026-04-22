79b488d5f0cf92ac1d430ca5b18c1d8465e49e76
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-04-22 09:37:00 +0530

    /

---
+++ Builtin.py
@@ -0,0 +1,28 @@
+# len()
+s = "hello"
+print(len(s))  
+
+# max() / min()
+arr = [5, 2, 9, 1]
+
+print(max(arr))  # 9
+print(min(arr))  # 1
+
+# sum() (dont work on strings)
+arr = [1,2,3,4]
+print(sum(arr))  # 10
+
+# sorted()
+arr = [3,1,4,2]
+print(sorted(arr))  # [1,2,3,4]
+
+# type()
+x = 5
+print(type(x))  # <class 'int'>
+
+# Type Conversion
+int("5")      # 5
+float("3.5")  # 3.5
+str(10)       # "10"
+list("abc")   # ['a','b','c']
+
