fe34d8fa246f3792b23374bb97370ece8cf27260
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-04-23 21:59:25 +0530

    /

--- Dictionary.py
+++ Dictionary.py
@@ -9,3 +9,14 @@ for ch in s:
         d[ch] = 1
 print(d);
 
+
+#2: Take a list of numbers and count frequency of each number using dictionary.
+nums = list(map(int, input("Enter your number : ").split()))
+d = {}
+for i in nums:
+    if i in d:
+        d[i] = d[i] + 1
+    else:
+        d[i] = 1
+print(d)
+        
