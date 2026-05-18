fe5d8066da2fe07f4e9d00318b76e674a97b6c60
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-05-18 20:42:58 +0530

    funcs started

---
+++ Functions.py
@@ -0,0 +1,10 @@
+# 🔹 What is a function?
+
+# A function is: a block of code that you can reuse
+# def function_name():
+#     # code
+
+# def greet(name):
+#     print("Hello", name)
+# Call:
+# greet("Rahul")
