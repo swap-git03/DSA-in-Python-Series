c62b5dcb242574d59978373138e6e4b5a89a8483
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-05-29 00:00:47 +0530

    /

--- conditionals.py
+++ conditionals.py
@@ -17,4 +17,30 @@ elif num2>=num1 and num2>=num3:
     print(f"num {num2} is greated")
 else:
     print(f"num {num3} is greated")
-    
+
+# 13. Check if a year is a leap year.
+year = int(input("Enter your year : "))
+if (year % 4== 0) and (year % 100 != 0) or (year % 400 ==0):
+    print("Year is a leap year")
+else:
+    print("Not a leap year")
+
+# 14. Input marks and print grade: A (90+), B (75+), C (60+), D (45+), F (below 45).
+marks = int(input("Enter marks"))
+if marks >= 90:
+    print("Grade is A")
+elif marks >= 75:
+    print("Grade is B")
+elif marks >= 60:
+    print("Grade is C")
+elif marks >= 45:
+    print("Grade is D")
+else:
+    print("F")
+
+# 15. Check if a number is divisible by both 3 and 5.
+num = int(input('Enter a number: '))
+if (num % 3 == 0) and (num % 5 == 0):
+    print(f"{num}Divisible by 3 and 5")
+else:
+    print(f"{num}NOT Divisible by 3 and 5")
