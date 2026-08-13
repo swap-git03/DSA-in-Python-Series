95eece41cdd7ef17e6c4c54c62c4a2f0a1ff847a
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-08-13 15:28:17 +0530

    Refactor Dictionary.py to improve print formatting and add examples for dictionary methods

--- Dictionary.py
+++ Dictionary.py
@@ -40,16 +40,79 @@ print(student)
 # Q8 Print all keys.
 
 for i in student:
-print(i)
+    print(i)
 
 # Q9 Print all values.
 
 for i in student.values():
-print(i)
+    print(i)
 
 # Q10 Print every key and value using .items()
 
 for key,value in student.items():
-print(key,value)
+    print(key,value)
 
-# Do all 10 together.
+student = {
+    "name": "Rahul",
+    "age": 22,
+    "course": "BDA"
+}
+
+
+# Q1 Use get() to retrieve "name".
+print(student.get("name"))
+
+# Q2 Use get() to retrieve "salary".
+print(student.get("salary"))
+
+# Q3 Use get() to retrieve "salary" but return 0 if it doesn't exist.
+print(student.get("salary",0))
+
+# Q4 Use update() to add:
+student.update({"city":"Mumbai", "phone":9999999999})
+print(student)
+
+
+# Q5 Use update() to change:
+# age → 23
+student.update({"age":23})
+print(student)
+
+# Q6 — Nested Dictionary
+student = {
+    "name": "Rahul",
+    "details": {
+        "age": 22,
+        "course": "BDA"
+    }
+}
+# Print the course.
+print(student["details"]["course"])
+
+# Q7 Change the nested age from 22 to 23.
+student["details"]["age"]=23
+print(student)
+
+# Q8 Create this:
+students = {
+    "student1": {
+        "name": "Rahul",
+        "age": 22
+    },
+    "student2": {
+        "name": "Amit",
+        "age": 24
+    }
+}
+# Print "Amit".
+print(students["student2"]["name"])
+# Q9 Print Rahul's age.
+print(students["student1"]["age"])
+
+# Q10 — Using a loop, print:
+for key, value in students.items():
+    print(key, value["name"])
+# student1 Rahul
+# student2 Amit
+
+# This last one is where nested dictionaries + .items() come together.
