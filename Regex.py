50c9acf60db104030b74e6caed8d14986ba0eb36
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-08-27 23:29:20 +0530

    Add regex example for extracting phone numbers from text

--- Regex.py
+++ Regex.py
@@ -9,3 +9,15 @@
 
 # search() : Finds the first match.
 # findall(): Finds all matches.
+
+import re
+text = """
+Customer Rahul
+Phone: 9876543210
+
+Customer Amit
+Phone: 8765432109
+"""
+phones = re.findall(r"\d{10}", text)
+
+print(phones)
