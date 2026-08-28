51f49e68577467dff0fb7d7f3a2724b1b13748ff
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-08-28 09:39:24 +0530

    Add regex examples for string matching, number extraction, and replacements

--- Regex.py
+++ Regex.py
@@ -50,3 +50,81 @@ print(res)
 text = "abc123@#$xyz"
 res = re.findall(r"[^a-zA-Z0-9]",text)
 print(res)
+
+
+# Q7 — Check whether strings start with "Python"
+texts = ["Python is easy", "I love Python", "Python123"]
+
+for text in texts:
+    result = re.match(r"Python", text)
+
+    if result:
+        print(text, "→ Yes")
+    else:
+        print(text, "→ No")
+
+# Q8 — Check whether strings end with ".com"
+
+texts = [
+    "google.com",
+    "gmail.com",
+    "google.in",
+    "example.com.in"
+]
+
+for text in texts:
+    result = re.search(r"\.com$", text)
+
+    if result:
+        print(text, "→ Yes")
+    else:
+        print(text, "→ No")
+
+# Q9 — Find cat or dog
+text = "I have a cat, dog, cow and another cat"
+result = re.findall(r"cat|dog", text)
+print(result)
+
+
+# Q10 — Find first sequence of digits
+text = "My employee ID is BDA12345"
+result = re.search(r"\d+", text)
+print(result.group())
+
+# Q11 — Extract all marks
+text = "Marks: 85, 92, 76, 90"
+result = re.findall(r"\d+", text)
+print(result)
+
+# Q12 — finditer()
+text = "I have 123 apples and 456 oranges"
+results = re.finditer(r"\d+", text)
+for match in results:
+    print("Value:", match.group())
+    print("Starting position:", match.start())
+
+
+# Q13 — Replace numbers
+text = "My PIN is 1234"
+result = re.sub(r"\d+", "XXXX", text)
+print(result)
+
+
+# Q14 — Split using multiple separators
+text = "apple,banana;orange|mango"
+result = re.split(r"[,;|]", text)
+print(result)
+
+
+text = "Rahul has 25 apples, Amit has 30 oranges and Priya has 15 bananas."
+# A. Extract numbers
+numbers = re.findall(r"\d+", text)
+print(numbers)
+
+# B. Extract names
+names = re.findall(r"[A-Z][a-z]+", text)
+print(names)
+
+# C. Replace numbers
+new_text = re.sub(r"\d+", "XX", text)
+print(new_text)
