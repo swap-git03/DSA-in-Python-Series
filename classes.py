2bf9cc3fa4c7792103bd89eb8381468a0b3605be
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-09-03 12:33:06 +0530

    /

--- classes.py
+++ classes.py
@@ -12,3 +12,24 @@ emp2 = employee("king", 12)
 
 emp1.display()
 emp2.display()
+
+
+
+class Employee:
+
+    company = "ABC"
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def display(self):
+        print(self.name, self.salary)
+
+    @classmethod
+    def change_company(cls, company):
+        cls.company = company
+
+    @staticmethod
+    def is_valid_salary(salary):
+        return salary > 0
